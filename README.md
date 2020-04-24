b40a5a84b0116ae41fcf324b2a81858c02146a69
Ben Pirt <ben@pirt.co.uk>
2020-04-24 12:42:32 +0100

    Initial spike commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PDF Generator
+
+This is a simple microservice that accepts html input and returns a PDF. It uses the serverless framework with Node.js and uses Chrome with Puppeteer to generate the PDF.
+
+## How to use
+
+Send a html document as a post to the endpoint and you'll get back a PDF
---
+++ handler.js
@@ -0,0 +1,44 @@
+"use strict";
+const chromium = require("chrome-aws-lambda");
+
+const pdfOptions = {
+  printBackground: true,
+  format: "A4",
+  margin: {
+    top: "20mm",
+    right: "20mm",
+    bottom: "20mm",
+    left: "20mm",
+  },
+};
+
+const launchBrowser = async () => {
+  return await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+  });
+};
+
+const convertDoc = async (doc) => {
+  const browser = await launchBrowser();
+  const page = await browser.newPage();
+  await page.setContent(doc);
+  const pdf = await page.pdf(pdfOptions);
+  await browser.close();
+  return pdf;
+};
+
+module.exports.convert = async (event) => {
+  const body = event.isBase64Encoded
+    ? new Buffer(event.body, "base64").toString()
+    : event.body;
+  const pdf = await convertDoc(body);
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/pdf" },
+    body: pdf.toString("base64"),
+    isBase64Encoded: true,
+  };
+};
